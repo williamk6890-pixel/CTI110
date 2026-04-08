4f4ea12eab65bfd5c6457b9b9d48cc0894126529
williamk6890-pixel <williamk6890@student.faytechcc.edu>
2026-04-07 22:54:15 -0400

    Add files via upload

---
+++ .py
@@ -0,0 +1,17 @@
+# Kiara Williams
+# 4/7/2026
+# P1LAB1
+# a program that allows the user to enter their first name and last name. Also displaying a message welcoming the user to this course.
+
+name1 = input("Enter your first name: ")
+name2 = input("Enter your last name: ")
+print("Hello,"+ name1, name2 +  "!" "Welcome to CTI-110")
+
+
+
+
+
+
+
+
+
