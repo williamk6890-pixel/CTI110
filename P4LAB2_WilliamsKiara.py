57c478db769f5760a61e23983874c97c3511e565
williamk6890-pixel <williamk6890@student.faytechcc.edu>
2026-06-04 21:56:22 -0400

    Add files via upload

---
+++ P4LAB2_WilliamsKiara.py
@@ -0,0 +1,44 @@
+num = int(input("Enter an integer: "))
+if num < 0:
+    print("Negative values are not allowed.")
+
+print(f"{num} x {1} = {num * 1}")
+print(f"{num} x {2} = {num * 2}")
+print(f"{num} x {3} = {num * 3}")
+print(f"{num} x {4} = {num * 4}")
+print(f"{num} x {5} = {num * 5}")
+print(f"{num} x {6} = {num * 6}")
+print(f"{num} x {7} = {num * 7}")
+print(f"{num} x {8} = {num * 8}")
+print(f"{num} x {9} = {num * 9}")
+print(f"{num} x {10} = {num * 10}")
+print(f"{num} x {11} = {num * 11}")
+print(f"{num} x {12} = {num * 12}")
+
+
+choice = input("\nWould you like to run the program again?: ").strip().lower()
+if choice == "no":
+        print("Exiting Program...")
+if choice == "yes":
+     while True:
+           num = int(input("Enter an integer: "))
+           print(f"{num} x {1} = {num * 1}")
+           print(f"{num} x {2} = {num * 2}")
+           print(f"{num} x {3} = {num * 3}")
+           print(f"{num} x {4} = {num * 4}")
+           print(f"{num} x {5} = {num * 5}")
+           print(f"{num} x {6} = {num * 6}")
+           print(f"{num} x {7} = {num * 7}")
+           print(f"{num} x {8} = {num * 8}")
+           print(f"{num} x {9} = {num * 9}")
+           print(f"{num} x {10} = {num * 10}")
+           print(f"{num} x {11} = {num * 11}")
+           print(f"{num} x {12} = {num * 12}")
+
+   
+
+
+
+
+
+
